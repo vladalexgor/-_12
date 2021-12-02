8663ebc04363e073dc40a6859d779ac781bb076f
vladalexgor@ya.ru <vladalexgor@ya.ru>
2021-12-03 00:02:41 +0300

    Исправлен код на более простой вид

--- Задание_12/Program.cs
+++ Задание_12/Program.cs
@@ -16,55 +16,51 @@ namespace Задание_12
         {
             Console.WriteLine("Программа находит длину окружности и ее площадь по заданному радиусу. Определяет принадлежность точки с введенными координатами кругу.");
             Console.WriteLine("Введите радиус окружности:");
-            Circle.Radius = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Длина окружности равна: L = {0:f2}", Circle.CircleLength());
-            Console.WriteLine("Площадь круга равна: S = {0:f2}", Circle.Area());
-            Console.WriteLine("Введите поочередно координаты (x0; y0) центра окружности:");
-            double x0 = Convert.ToDouble(Console.ReadLine());
-            double y0 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введенные координаты центра окружности: ({0} ; {1})", x0, y0);
-            Console.WriteLine("Введите поочередно координаты (x1; y1) точки:");
-            double x1 = Convert.ToDouble(Console.ReadLine());
-            double y1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введенные координаты точки: ({0} ; {1})", x1, y1);
-            Circle.PointBelongingCircle(x0, y0, x1, y1);
-            Console.WriteLine("Для завершения программы нажмите любую клавишу.");
-            Console.ReadKey();
+            try
+            {
+                double r = Convert.ToDouble(Console.ReadLine());
+                if (r > 0)
+                {
+                    Console.WriteLine("Длина окружности равна: L = {0:f2}", Circle.CircleLength(r));
+                    Console.WriteLine("Площадь круга равна: S = {0:f2}", Circle.Area(r));
+                    Console.WriteLine("Введите поочередно координаты (x0; y0) центра окружности:");
+                    double x0 = Convert.ToDouble(Console.ReadLine());
+                    double y0 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введенные координаты центра окружности: ({0} ; {1})", x0, y0);
+                    Console.WriteLine("Введите поочередно координаты (x1; y1) точки:");
+                    double x1 = Convert.ToDouble(Console.ReadLine());
+                    double y1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введенные координаты точки: ({0} ; {1})", x1, y1);
+                    Circle.PointBelongingCircle(x0, y0, x1, y1, r);
+                    Console.WriteLine("Для завершения программы нажмите любую клавишу.");
+                }
+                else
+                {
+                    Console.WriteLine("Радиус должен быть задан значением больше нуля.");
+                }
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
         }
     }
 
     public static class Circle
     {
-        static double radius;
-        public static double Radius
-        {
-            set
-            {
-                if (value > 0)
-                {
-                    radius = value;
-                }
-                else
-                {
-                    Console.WriteLine("Значение радуса должно быть больше нуля.");
-                }
-            }
-            get
-            {
-                return radius;
-            }
-        }
-        public static double CircleLength()
+       public static double CircleLength(double radius)
         {
             double circleLength = 2 * Math.PI * radius;
             return circleLength;
         }
-        public static double Area()
+        public static double Area(double radius)
         {
             double area = Math.PI * radius * radius;
             return area;
         }
-        public static void PointBelongingCircle(double x0, double y0, double x, double y)
+        public static void PointBelongingCircle(double x0, double y0, double x, double y, double radius)
         {
             double distanceBetweenCoordinates = Math.Sqrt(Math.Pow((x0 - x), 2) + Math.Pow((y0 - y), 2));
             if (distanceBetweenCoordinates <= radius)
